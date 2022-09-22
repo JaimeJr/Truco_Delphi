f52a55a8372e6412ffb12f4ca0974830290e3a01
Jaime Junior <jaime-jr98@hotmail.com>
2022-09-22 20:25:14 -0300

    Adicionado novo projeto

---
+++ Truco/frmPrincipal.pas
@@ -0,0 +1,27 @@
+unit frmPrincipal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls;
+
+type
+  TfrmTelaPrincipal = class(TForm)
+    pnlFundo: TPanel;
+    pnlCabecalho: TPanel;
+    pnlRodape: TPanel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmTelaPrincipal: TfrmTelaPrincipal;
+
+implementation
+
+{$R *.dfm}
+
+end.
